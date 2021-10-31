3837def6cfbf83285eb818b9fd6d30ddb2676eb6
sunil batham <75170529+sunil17023@users.noreply.github.com>
2021-10-31 22:18:39 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -13,7 +13,7 @@ def man():
     return render_template("home.html")
 
 
-@app.route("/after", methods=['GET', "POST"])
+@app.route("/after", methods=["POST"])
 def home():
     data1 = request.form["a"]
     data2 = request.form["b"]
