57698b2ecaa5ff99abddd0f0d8fb1004555902f3
sunil batham <75170529+sunil17023@users.noreply.github.com>
2021-11-06 16:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 model = pickle.load(open("iris_model_deploy.pkl", "rb"))
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder= "https://github.com/sunil17023/Iris_Deployment/tree/main/static")
 
 
 @app.route("/")
